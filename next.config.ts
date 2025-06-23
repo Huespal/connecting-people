9259c2005c13871f83d0f4b1a2f81af7295aacd1
Huespal <Huespal@users.noreply.github.com>
2025-06-23 18:30:09 +0200

    build: adapt next config for a server without CORS setup

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  // The proxy is added to bypass the missing CORS configuration in the server.
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+      }
+    ]
+  }
+};
 
 export default nextConfig;
